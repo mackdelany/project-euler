da7092af86435d712b0777ec49511fe89e7f6374
mackdelany <mackdelany@gmail.com>
2019-01-24 16:27:25 +1300

    Update euler13.py

--- euler13.py
+++ euler13.py
@@ -3,6 +3,9 @@
 # Work out the first ten digits of the sum of the one-hundred 50-digit numbers
 # in the file 'euler13_numbers.txt'
 
+import time
+start = time.time()
+
 filename = 'euler13nums.txt'
 with open(filename, "r") as nums:
 
