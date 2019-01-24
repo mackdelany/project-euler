643e120ee81c29ef48899ddcd5a827b23b96cd78
mackdelany <mackdelany@gmail.com>
2019-01-24 16:32:25 +1300

    Update euler65.py

--- euler65.py
+++ euler65.py
@@ -2,7 +2,8 @@
 ##########################
 # Find the sum of digits in the numerator of the 100th convergent of the continued fraction for e.
 
-# create n100 terms matrix
+import time
+start = time.time()
 
 terms = [2]
 
