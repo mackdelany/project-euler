57f44fa43c731073505f140b32d7ac656b0c03cd
mackdelany <mackdelany@gmail.com>
2019-01-24 16:29:41 +1300

    Update euler25.py

--- euler25.py
+++ euler25.py
@@ -3,7 +3,6 @@
 # What is the index of the first term in the Fibonacci sequence to contain 1000 digits?
 
 import time
-
 start = time.time()
 
 fibs = [1,1]
@@ -13,8 +12,6 @@ while len(str(fibs[-1])) < 1000:
     fibs.append(fibs[-1]+fibs[-2])
     index.append(index[-1] + 1)
 
-print(fibs)
-print(index)
 print("The answer is " + str(index[-1]))
 
 print("It took " + str(time.time() - start) + " seconds")
