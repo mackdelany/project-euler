e9af23acbd957369f80bfeab165e021b2f8b764f
mackdelany <mackdelany@gmail.com>
2019-01-24 16:26:58 +1300

    Update euler1.py

--- euler1.py
+++ euler1.py
@@ -6,8 +6,10 @@
 #The sum of these multiples is 23.
 #Find the sum of all the multiples of 3 or 5 below 1000.
 
-numbers = list(range(1,1000))
+import time
+start = time.time()
 
+numbers = list(range(1,1000))
 multiples = []
 
 for items in numbers:
