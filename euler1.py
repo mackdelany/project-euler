6f526b7797ab2fb54d0d0c18e7cec8219f08fdde
mackdelany <mackdelany@gmail.com>
2019-02-09 14:07:20 -0500

    Update euler1.py

--- euler1.py
+++ euler1.py
@@ -2,9 +2,9 @@
 ##########################
 # Multiples of 3 and 5
 
-#If we list all the natural numbers below 10 that are multiples of 3 or 5, we get 3, 5, 6 and 9.
-#The sum of these multiples is 23.
-#Find the sum of all the multiples of 3 or 5 below 1000.
+# If we list all the natural numbers below 10 that are multiples of 3 or 5, we get 3, 5, 6 and 9.
+# The sum of these multiples is 23.
+# Find the sum of all the multiples of 3 or 5 below 1000.
 
 import time
 start = time.time()
@@ -12,10 +12,10 @@ start = time.time()
 numbers = list(range(1,1000))
 multiples = []
 
-for items in numbers:
-    if items % 3 == 0:
+for naturalNumber in numbers:
+    if naturalNumber % 3 == 0:
         multiples.append(items)
-    elif items % 5 == 0:
+    elif naturalNumber % 5 == 0:
         multiples.append(items)
 
 sumOfMultiples = sum(multiples)
